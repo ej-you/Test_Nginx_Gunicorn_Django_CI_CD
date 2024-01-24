f083d052a134b62144222b68279f86b3c6c5bcfe
Danil-114195722 <ejyou.user@gmail.com>
2024-01-24 09:27:11 +0300

    Added rest to requirements.txt

---
+++ backend/HelloWorld/hello/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+#
+# from . import views
+#
+#
+# urlpatterns = [
+#     path('', views, name="index"),
+#     path('hello-world/', views, name="hello-world"),
+#     path('users/', views, name="users"),
+# ]
