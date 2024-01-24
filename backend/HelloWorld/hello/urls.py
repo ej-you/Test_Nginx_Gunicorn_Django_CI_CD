a85d5a4785708cc0db98d0e65329a271383cfc77
Danil-114195722 <ejyou.user@gmail.com>
2024-01-24 20:35:39 +0300

    Added urls and views

--- backend/HelloWorld/hello/urls.py
+++ backend/HelloWorld/hello/urls.py
@@ -1,10 +1,10 @@
-# from django.urls import path
-#
-# from . import views
-#
-#
-# urlpatterns = [
-#     path('', views, name="index"),
-#     path('hello-world/', views, name="hello-world"),
-#     path('users/', views, name="users"),
-# ]
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('hello-world/', views.hello_world, name="hello-world"),
+    path('users/', views.users, name="users"),
+]
--- backend/HelloWorld/hello/views.py
+++ backend/HelloWorld/hello/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+def index(request):
+    return Response(data='<h1>INDEX</h1>', status=status.HTTP_200_OK)
+
+
+def hello_world(request):
+    return Response(data='Hello world!', status=status.HTTP_200_OK)
+
+
+def users(request):
+    data = {
+        "user1": 'user1',
+    }
+    return Response(data=data, status=status.HTTP_200_OK)
