06c4e9f4b6aee685b2c848e1b597f4c7e59a5d0f
Danil-114195722 <ejyou.user@gmail.com>
2024-01-24 21:34:38 +0300

    Added model to tests

--- backend/HelloWorld/hello/models.py
+++ backend/HelloWorld/hello/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class CiCdUser(models.Model):
+    email = models.EmailField(max_length=150, null=False, blank=False)
+    password = models.CharField(max_length=255, null=False, blank=False)
+    bio = models.TextField(null=True)
