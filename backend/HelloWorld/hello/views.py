4987442196abe0e45ad79fbc27d0ba4635ec080e
Danil-114195722 <ejyou.user@gmail.com>
2024-01-24 21:34:58 +0300

    Added template and fixed views to tests

--- backend/HelloWorld/hello/views.py
+++ backend/HelloWorld/hello/views.py
@@ -1,19 +1,50 @@
 from rest_framework import status
-from rest_framework.response import Response
+from django.shortcuts import render
+
+from .models import CiCdUser
 
 # Create your views here.
 
 
 def index(request):
-    return Response(data='<h1>INDEX</h1>', status=status.HTTP_200_OK)
+    data = {
+        "title": "Index",
+        "body": 'INDEX',
+    }
+
+    return render(request, 'index.html', context=data, status=status.HTTP_200_OK)
 
 
 def hello_world(request):
-    return Response(data='Hello world!', status=status.HTTP_200_OK)
+    data = {
+        "title": "Hello world",
+        "body": 'Hello world',
+    }
+
+    return render(request, 'index.html', context=data, status=status.HTTP_200_OK)
 
 
 def users(request):
+    user_queryset = CiCdUser.objects.all()
+
+    user_list = [
+        {
+            "email": user.email,
+            "password": user.password,
+            "bio": bio if (bio := user.bio) else "!!!Doesn't exist!!!",
+        }
+        for user in user_queryset
+    ]
+
     data = {
-        "user1": 'user1',
+        "title": "Users",
+        "body": user_list,
     }
-    return Response(data=data, status=status.HTTP_200_OK)
+
+    return render(request, 'index.html', context=data, status=status.HTTP_200_OK)
+
+
+# insert into ci_cd_users (email, password, bio) VALUES
+# ('ejyou.user@gmail.com', '123qwerty', 'about me'),
+# ('test@test.com', 'password', 'nothing'),
+# ('sample@sample.ru', 'easy_pass', NULL);
